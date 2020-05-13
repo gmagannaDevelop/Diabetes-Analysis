a710cfeef5a96f844bee141661b696b3ae4436e9
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-05-13 18:29:50 -0500

    :sparkles: Added glycaemic variability function.
    This only includes Medtronic's naive method : σ/μ. More methods to come later on.

--- Utils.py
+++ Utils.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
 
 # Annotations :
-from typing import List, Dict, Callable, NoReturn, Any, Optional
+from typing import List, Dict, Callable, NoReturn, Any, Optional, Union
 
 # Data and numerical :
 import numpy as np
 import pandas as pd
+import scipy
 
 # Plotting :
 import matplotlib.pyplot as plt
@@ -24,7 +25,7 @@ plt.style.use('seaborn')
 plt.rcParams['figure.figsize'] = (15, 8)
 
 
-# In[2]:
+# In[11]:
 
 
 def comparative_hba1c_plot(
@@ -93,7 +94,7 @@ def comparative_hba1c_plot(
 ##
 
 
-# In[9]:
+# In[3]:
 
 
 def proportions_visualiser(
@@ -142,7 +143,7 @@ def proportions_visualiser(
     valid_kinds = ["TIR", "TBR", "TAR"]
     
     if "low" not in limits.keys() or "high" not in limits.keys():
-        raise Exception(f"WUjuu")
+        raise Exception(f"limits.keys() should be ['low', 'high'] not {limits.keys()}")
     
     titles = {
         "TIR": f"Time In Range [{limits['low']},{limits['high']})",
@@ -173,22 +174,101 @@ def proportions_visualiser(
 ##    
 
 
-# In[12]:
+# In[26]:
+
+
+def glycaemic_variability(
+    df: pd.core.frame.DataFrame,
+    colum_name: str = "Sensor Glucose (mg/dL)",
+    windows: Dict[str,int] = {
+        "weekly": 7,
+        "monthly": 30
+    },
+    kind: str = "NAIVE"
+) -> NoReturn:
+    """
+    
+    """
+    
+    methods = {
+        "NAIVE": lambda x: 100 * pd.Series.std(x) / pd.Series.mean(x)
+    }
+    
+    kind = kind.upper()
+    
+    if kind in methods.keys():
+        _statistic = df.groupby(df.index.date)[colum_name].apply(methods[kind])
+    
+    # Plot the main series :
+    _statistic.plot(**{"label":"daily"})
+    
+    # Plot the moving averages :
+    for key, value in windows.items():
+        ax = _statistic.rolling(value).mean().plot(**{"label":key})
+        
+    _global_mean = _statistic.mean()
+    plt.axhline(_global_mean, **{"label": f"mean = {round(_global_mean,1)}", "c": "blue"})
+    plt.legend()
+    plt.title(f"Glycaemic Variability, assesment method : {kind}")
+##
+
+
+# In[4]:
+
+
+def dist_plot(series: pd.core.series.Series, dropna: bool = True) -> NoReturn:
+    """
+        Given a pandas Series, generate a descriptive visualisation 
+        with a boxplot and a histogram with a kde.
+        By default, this function drops `nan` values. If you desire to
+        handle them differently, you should do so beforehand and/or
+        specify dropna=False.
+    """
+    
+    if dropna:
+        series = series.dropna()
+    
+    quarts = scipy.stats.mstats.mquantiles(series, [0.001, 0.25, 0.5, 0.75, 0.975])
+    
+    f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.25, .75)})
+    sns.boxplot(series, ax=ax_box)
+    sns.stripplot(series, color="orange", jitter=0.2, size=2.5, ax=ax_box)
+    sns.distplot(series, ax=ax_hist, kde=True)
+    ax_hist.axvline(series.mean())
+    ax_hist.set_xticks(quarts)
+    #ax_box.set(xlabel=f'Mean value : {int(series.mean())}')
+    plt.title(f"Glycaemic Distribution μ = {int(series.mean())}, σ = {int(series.std())}")
+##
+
+
+# In[5]:
 
 
 #data = import_csv("preprocessed/CareLink-19-apr-2020-3-months.csv")
 
 
-# In[7]:
+# In[6]:
+
+
+#dist_plot(data["Sensor Calibration BG (mg/dL)"])
+
+
+# In[30]:
 
 
 #comparative_hba1c_plot(data, kind="mean")
 
 
-# In[11]:
+# In[16]:
 
 
 #proportions_visualiser(data)
+
+
+# In[27]:
+
+
+#glycaemic_variability(data)
 
 
 # In[ ]:

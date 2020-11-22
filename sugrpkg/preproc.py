317eb31b77cc76626d5d2c04b4f839447cc7cb41
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-11-22 21:51:13 +0100

    :fire: Removed legacy function
    In sugrpkg.preproc, the old hybrid interpolator should have been removed a long time ago. It was no longer used as new_hybrid_interpolator outperforms it and is more legible.

--- sugrpkg/preproc.py
+++ sugrpkg/preproc.py
@@ -132,82 +132,6 @@ def merge_on_duplicate_idx(
     return y
 
 
-##
-
-# @time_log("logs/preprocessing.jsonl")
-def hybrid_interpolator(
-    data: pd.core.series.Series,
-    mean: float = None,
-    limit: float = None,
-    methods: List[str] = ["linear", "spline"],
-    weights: List[float] = [0.65, 0.35],
-    direction: str = "forward",
-    order: int = 2,
-) -> pd.core.series.Series:
-    """
-    Return a pandas.core.series.Series instance resulting of the weighted average
-    of two interpolation methods.
-
-    Model:
-        φ = β1*method1 + β2*method2
-
-    Default:
-        β1, β2 = 0.6, 0.4
-        method1, method2 = linear, spline
-
-    Weights are meant to be numbers from the interval (0, 1)
-    which add up to one, to keep the weighted sum consistent.
-
-    limit_direction : {‘forward’, ‘backward’, ‘both’}, default ‘forward’
-    If limit is specified, consecutive NaNs will be filled in this direction.
-
-    If the predicted φ_i value is outside of the the interval
-    ( (mean - limit), (mean + limit) )
-    it will be replaced by the linear interpolation approximation.
-
-        limit = 2 * data.std()
-
-    This function should have support for keyword arguments, but is yet to be implemented.
-    """
-    predictions: List[float] = []
-
-    if not np.isclose(sum(weight for weight in weights), 1):
-        raise Exception("Sum of weights must be equal to one!")
-
-    for met in methods:
-        if (met == "spline") or (met == "polynomial"):
-            predictions.append(
-                data.interpolate(method=met, order=order, limit_direction=direction)
-            )
-        else:
-            predictions.append(data.interpolate(method=met, limit_direction=direction))
-
-    linear: pd.core.series.Series = predictions[0]
-    spline: pd.core.series.Series = predictions[1]
-    hybrid: pd.core.series.Series = (
-        weights[0] * predictions[0] + weights[1] * predictions[1]
-    )
-
-    corrected: pd.core.series.Series = copy.deepcopy(hybrid)
-
-    if not mean:
-        mean = data.mean()
-    if not limit:
-        limit = 2 * data.std()
-
-    for idx, val in zip(hybrid[np.isnan(data)].index, hybrid[np.isnan(data)]):
-        if (val > mean + limit) or (val < mean - limit):
-            corrected[idx] = linear[idx]
-
-    # df = copy.deepcopy(interpolated)
-    # print(df.isnull().astype(int).groupby(df.notnull().astype(int).cumsum()).sum())
-
-    return corrected
-
-
-##
-
-
 def new_hybrid_interpolator(
     data: pd.core.series.Series,
     methods: Dict[str, float] = {"linear": 0.65, "spline": 0.35},
@@ -240,11 +164,12 @@ def new_hybrid_interpolator(
     return reduce(lambda x, y: x + y, resampled.values())
 
 
-##
-
-
 def add_time_periodicity(df: pd.DataFrame) -> NoReturn:
-    """"""
+    """ Add x(t) and y(t) defined as cosine and 
+    sine of the minutes of the day. 
+    These there are 1440 minutes in a day, 
+    so the period T is set to 1439 (because we start at 0)
+    """
     # Coulmns to capture daily periodicity :
     T = 1439
     min_res_t_series = pd.Series(df.index.hour * 60 + df.index.minute)
@@ -272,9 +197,6 @@ def compute_time_periodicity(df: pd.DataFrame) -> NoReturn:
     )
     _tmp.index = df.index
     return _tmp
-
-
-##
 
 
 def import_csv(

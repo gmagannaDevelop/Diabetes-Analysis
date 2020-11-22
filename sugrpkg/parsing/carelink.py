cd3a7ee9a068b7598ef08f3fbc464c0f8f610819
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-11-22 23:33:45 +0100

    :bug: Added a integrity check for parsing.
    sugrpkg.parsing.carelink.parse_carelink was built under the assumption of homogenity on columns. It seems like Medtronic's site is not reliable. I cannot figure out why 18 out of 66 files have an extra column 'BLE Network Device' which is completely void, on all of them. Hence, adding a dtype to the parsing specs in sugrpkg.assets.carelink_parsing.carelink_parse_kw would be guessing which is a bad approach. Taking advantage of the python 'contains' operation's efficiency which is O(1), we first check if the column has a dtype defined on our parsing scheme, otherwise we skip setting its dtype.

--- sugrpkg/parsing/carelink.py
+++ sugrpkg/parsing/carelink.py
@@ -13,7 +13,8 @@ def parse_carelink(handle, addedcols: bool = True):
 
     frame = pd.read_csv(handle, **parsing.kwargs)
     for column in frame.iteritems():
-        frame.loc[:, column[0]] = column[1].astype(parsing.dtypes.no_sparse[column[0]])
+        if column[0] in parsing.dtypes.no_sparse.keys():
+            frame.loc[:, column[0]] = column[1].astype(parsing.dtypes.no_sparse[column[0]])
 
     for column in parsing.timedelta.cols:
         frame.loc[:, column] = frame.loc[:, column].fillna("").apply(pd.to_timedelta)

04bf954755a01492ed5e66bc9f2c569cb9c6cb7f
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-11-22 21:53:23 +0100

    :boom: Added carelink parsing.
    It is fully functional. Next steps will include writing a parser for other formats and pool parsers to achieve the ultimate goal: universal glycaemic data format parsing.

---
+++ sugrpkg/parsing/carelink.py
@@ -0,0 +1,25 @@
+import copy
+import pandas as pd
+
+from ..utils.customobjs import Path as path, objdict as odict
+
+from ..assets.carelink_parsing import carelink_parse_kw as parsing
+
+
+def parse_carelink(handle, addedcols: bool = True):
+    """ """
+    if addedcols:
+        parsing.dtypes.no_sparse.update(parsing.dtypes.added)
+
+    frame = pd.read_csv(handle, **parsing.kwargs)
+    for column in frame.iteritems():
+        frame.loc[:, column[0]] = column[1].astype(parsing.dtypes.no_sparse[column[0]])
+
+    for column in parsing.timedelta.cols:
+        frame.loc[:, column] = frame.loc[:, column].fillna("").apply(pd.to_timedelta)
+
+    return frame
+
+
+if __name__ == "__main__":
+    pass

32758a39f0f86b09645e32b4f8c9a29f729e2463
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2020-11-23 01:06:21 +0100

    :bug: Ducktaped Medtronic's basal mess
    For some weird reason Medtronic thought that it would be funny to include 'UNKNOWN RATE' in some files' Basal Rate columns. I added a small utility function which cleans this up and coerces the column's dtype to float.

--- sugrpkg/parsing/carelink.py
+++ sugrpkg/parsing/carelink.py
@@ -1,9 +1,22 @@
 import copy
+import numpy as np
 import pandas as pd
+from pandas.api.types import is_float_dtype
 
 from ..utils.customobjs import Path as path, objdict as odict
 
 from ..assets.carelink_parsing import carelink_parse_kw as parsing
+
+BASAL_COL = "Basal Rate (U/h)"
+
+
+def _ducktape_to_float(column: pd.Series):
+    """ """
+    return (
+        column.apply(lambda x: "" if pd.isna(x) else x)
+        .apply(lambda x: x if x.replace(".", "", 1).isdigit() else np.nan)
+        .astype(float)
+    )
 
 
 def parse_carelink(handle, addedcols: bool = True):
@@ -12,9 +25,13 @@ def parse_carelink(handle, addedcols: bool = True):
         parsing.dtypes.no_sparse.update(parsing.dtypes.added)
 
     frame = pd.read_csv(handle, **parsing.kwargs)
+    if not is_float_dtype(frame[BASAL_COL]):
+        frame.loc[:, BASAL_COL] = _ducktape_to_float(frame[BASAL_COL])
     for column in frame.iteritems():
         if column[0] in parsing.dtypes.no_sparse.keys():
-            frame.loc[:, column[0]] = column[1].astype(parsing.dtypes.no_sparse[column[0]])
+            frame.loc[:, column[0]] = column[1].astype(
+                parsing.dtypes.no_sparse[column[0]]
+            )
 
     for column in parsing.timedelta.cols:
         frame.loc[:, column] = frame.loc[:, column].fillna("").apply(pd.to_timedelta)
